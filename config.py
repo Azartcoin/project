5e1738b3d281c84baf5f01649d7d1b80302edeba
Azartcoin <159246589+Azartcoin@users.noreply.github.com>
2025-05-14 23:30:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+TOKEN = '7810391096:AAF8Ap7CwWXZCNd9jIzUCRQv9fsD5sgtNgQ'
+admins = [7052720939]
+admin_chat_id = -1002533219619
+client_id = '87794b651c034f4'
+client_secret = 'be16d8cd1f91596af83042e52f0add605fecc8e0'
+chat = 'https://t.me/+ihQlEU_whxViNmFk'
+channel = 'https://t.me/+Llb16inhMs8zYTk0'
+verif = 'Shawe_cvv'
+
+help_text = '''✅ Все активные команды чата
+
+
+
+💳 Данные карты - /card
+🏆 Топ за все время - /top
+🏆 Топ за месяц - /topm
+🏆 Топ за день - /topd
+👤 Мой профиль - /mp
+
+<a href="https://telegra.ph/LuxuryTeam-Manual-12-03">📚 Мануал для новичков</a>'''
---
+++ keyboards.py
@@ -0,0 +1,83 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+from config import *
+
+
+def keyboardMain(admin=False):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    if admin:
+        nkb.row(InlineKeyboardButton('Добавить профит➕', callback_data=f'addprofit'),InlineKeyboardButton('Добавить пустой профит⭕', callback_data=f'add_none'))
+        nkb.row(InlineKeyboardButton('Изменить текст card💳', callback_data=f'edit_card'),InlineKeyboardButton('Найти юзера🔎', callback_data=f'find_user'))
+        nkb.row(InlineKeyboardButton('Личный кабинет👤', callback_data='lc'),
+                InlineKeyboardButton('Наставники👨‍🏫', callback_data='teach_adm'))
+        #nkb.row(InlineKeyboardButton('Изменить свой тег', callback_data=f'edit_tag'))
+        nkb.row(InlineKeyboardButton('Чат💬', url=chat), InlineKeyboardButton('Канал выплат💰', url=channel))
+        nkb.row(InlineKeyboardButton('👠Создать анкету', callback_data='sozd_ank'))
+
+    else:
+        #nkb.row(InlineKeyboardButton('Изменить свой тег', callback_data=f'edit_tag'))
+        nkb.row(InlineKeyboardButton('Личный кабинет👤', callback_data='lc'), InlineKeyboardButton('Наставники', callback_data='teach'))
+        nkb.row(InlineKeyboardButton('Чат💬',url=chat), InlineKeyboardButton('Канал выплат💰',url=channel))
+        nkb.row(InlineKeyboardButton('👠Создать анкету', callback_data='sozd_ank'))
+    return nkb
+
+
+def keyboardCancel():
+    nkb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    nkb.row(KeyboardButton('Отмена❌'))
+    return nkb
+
+def keyboardCancel_Skip():
+    nkb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    nkb.row(KeyboardButton('Отмена❌'), KeyboardButton('Пропустить➡️'))
+    return nkb
+
+
+def keyboardAccept(user_id):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton('Принять✅',callback_data=f'acc_yes{user_id}'),InlineKeyboardButton('Отказ❌',callback_data=f'acc_no{user_id}'))
+    return nkb
+
+def keyboardReq():
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.add(InlineKeyboardButton('Отправить заявку✅',callback_data='send_req'))
+    return nkb
+
+def keyboardFINISH():
+    nkb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    nkb.row(KeyboardButton('Готово✅'),KeyboardButton('Отмена❌'))
+    return nkb
+
+def teach_keyboard(teachers):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    for i in teachers:
+        nkb.add(InlineKeyboardButton(i, callback_data=f'select_teach_{i}'))
+    nkb.add(InlineKeyboardButton('Назад⬅️', callback_data='back'))
+    return nkb
+
+
+def keyboardAcceptTeacher(user_id):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton('Принять✅',callback_data=f't_acc_yes{user_id}'),InlineKeyboardButton('Отказ❌',callback_data=f't_acc_no{user_id}'))
+    return nkb
+
+def keyboardBack():
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton('Назад',callback_data=f'back'))
+    return nkb
+
+def keyboardCard():
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton(f'@{verif}',url=f't.me/{verif}'))
+    return nkb
+
+def keyboardUnban(user_id):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton('Unban', callback_data=f'unban_{user_id}'))
+    return nkb
+
+def keyboardUnmute(user_id):
+    nkb = InlineKeyboardMarkup(resize_keyboard=True)
+    nkb.row(InlineKeyboardButton('Unmute', callback_data=f'unmute_{user_id}'))
+    return nkb
