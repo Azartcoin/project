cd25d8a94c4c0280b2cefcfdaa4bae6d43558bd9
Azartcoin <froloaroman@gmail.com>
2025-05-14 22:11:07 +0300

    Обновление порта и другие правки

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
 // Статические файлы
 app.use(express.static(path.join(__dirname, 'public')));
